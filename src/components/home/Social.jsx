0e1a17723fe1da4aa74e09d2c49d91fd9e41e4a5
deathwish07 <ayanaratharva@gmail.com>
2024-12-08 18:51:02 +0530

    profile

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -7,12 +7,8 @@ const Social = () => {
                 <i className="uil uil-linkedin"></i>
             </a>
 
-            <a href="https://github.com/avkumar" className="home__social-icon" target='_blank'>
+            <a href="https://github.com/SharanyaSrinivas" className="home__social-icon" target='_blank'>
                 <i className="uil uil-github"></i>
-            </a>
-
-            <a href="https://x.com/SaiKumarArava2" className="home__social-icon" target='_blank'>
-                <i className="uil uil-twitter"></i>
             </a>
 
         </div>
